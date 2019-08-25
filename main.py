7017ba445201eec1a30d655efaa76cd3cc96a329
tapanmeena <tapanmeena1998@gmail.com>
2019-08-25 20:17:53 +0530

    Add feature for Registring Face directly in main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 #libraries
+import cv2
+import sys
+import warnings
+import logging
 import os
-#For suppressing warnings.0 = all messages are logged (default behavior),1 = INFO messages are not printed,2 = INFO and WARNING messages are not printed,3 = INFO, WARNING, and ERROR messages are not printed
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
-# import tensorflow as tf
+import time
 import argparse
 from matplotlib import pyplot
 from matplotlib.patches import Rectangle
-import logging
-import cv2, sys
-import warnings
-from mtcnn.mtcnn import MTCNN
+import numpy as np
+'''
+	For disabling warning while importing tensorflow and MTCNN
+'''
 logging.disable(logging.WARNING)
 warnings.filterwarnings("ignore")
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #For suppressing warnings.0 = all messages are logged (default behavior),1 = INFO messages are not printed,2 = INFO and WARNING messages are not printed,3 = INFO, WARNING, and ERROR messages are not printed
+
+# import tensorflow as tf
+from mtcnn.mtcnn import MTCNN
 
 #make boxes on detected faces
 def make_boxes(filename, result_list):
@@ -95,16 +101,60 @@ def live_capture():
 			cv2.imshow("Face",img)
 			if(cv2.waitKey(1) & 0xFF==ord('q')):
 				break
+
+def Add_Face():
+	person_rollNumber = input("Person Roll Number : ")
+	folder = 'people/'+person_rollNumber
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+		input("Press Enter to Record Face ")
+
+		video = cv2.VideoCapture(0)
+		# detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+		# detector = cv2.CascadeClassifier('faces.xml')
+		detector = MTCNN()
+
+		photosTaken = 1
+
+		while photosTaken !=31:
+			check, frame = video.read()
+			faces = detector.detect_faces(frame)
+
+			if(len(faces) != 1):
+				pass
+
+			for result in faces:
+				x, y, w, h = result['box']
+
+				face = frame[y:y+h, x:x+w]
+				# create the shape
+				cv2.rectangle(frame,(x,y),(x+w,y+h),(0, 255,0), 2)
+				filePath = folder +'/' + str(photosTaken) + '.jpg'
+				cv2.imwrite(filePath, face)
+				print("Images Saved : " + str(photosTaken))
+		        # cv2.imwrite(name+'/'+str(pic_no)+'.jpg',cropped)
+				photosTaken += 1
+				cv2.imshow('Saved Face', face)
+			cv2.imshow('Face ', frame)
+			cv2.waitKey(100)
+
+
+		video.release()
+		cv2.destroyAllWindows()
+	else:
+		print("Person Record Already Exists")
+
+
 def argument_parse():
 	parser = argparse.ArgumentParser(description='Attendence Manager using face detection')
 	parser.add_argument('-i','--image',help='To process an image file',action='store')
 	parser.add_argument('-v','--video',help='To process an video file',action='store')
 	parser.add_argument('-c','--live',help='To process live capture',action='store_true')
-	# parser.add_argument('-t','--train',help='To train the model on image data',action='store_true')
+	parser.add_argument('-t','--train',help='To train the model on image data',action='store_true')
+	parser.add_argument('-a','--addFace',help='To Register New Face into Database',action='store_true')
 	
 	args = parser.parse_args()
 	if args.image:
-		# print(args.image)
 		image_file=args.image
 		find_face_in_image(image_file)
 	elif args.video:
@@ -112,10 +162,13 @@ def argument_parse():
 		find_face_in_video_file(video_file)
 	elif args.live:
 		live_capture()
-	# elif args.train:
-	# 	train_model()
+	elif args.train:
+		train_model()
+	elif args.addFace:
+		Add_Face()
 	else:
 		print("No arguments given. Use -h option for list of all arguments available.")
 
 if __name__ == '__main__':
 	argument_parse()
+	# GenDataSet()
